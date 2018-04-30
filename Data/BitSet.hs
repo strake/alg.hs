fd323cebe3a82a416fe78eacfb910671b445f542
M Farkas-Dyck <strake888@gmail.com>
2018-04-30 00:52:51 -0800

    rangeInclusive

--- Data/BitSet.hs
+++ Data/BitSet.hs
@@ -1,13 +1,19 @@
 module Data.BitSet where
 
-import Algebra
+import Control.Applicative
+import Control.Monad (guard)
 import Data.Bits
+import Data.List (concatMap)
+import Data.Maybe
 import Prelude ((==))
 import qualified Prelude as Base
+import Util.Bits
+
+import Algebra
 import Relation.Binary.Comparison
 
 newtype BitSet a = BitSet { bits :: a }
-  deriving (Base.Eq, Bits, Base.Read, Base.Show)
+  deriving (Base.Eq, Bits, FiniteBits, Base.Read, Base.Show)
 
 instance Bits a => Preord (BitSet a) where
     BitSet x ≤ BitSet y = x .&. y == x
@@ -23,3 +29,10 @@ instance Bits a => Monoid (BitSet a) where
     mappend = (<>)
     mempty = zeroBits
 instance Bits a => Group (BitSet a) where invert = complement
+
+rangeInclusive :: (PartialOrd a, Bits a) => a -> a -> [a]
+rangeInclusive = \ x y -> go y x <* guard (x ≤ y)
+  where go y x = x : concatMap (go y) (setBit x <$> setBits (y .&¬ x))
+
+(.&?¬) :: (PartialOrd a, Bits a) => a -> a -> Maybe a
+a .&?¬ b = a .&¬ b <$ guard (b ≤ a)
